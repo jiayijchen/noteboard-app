5b828e26dfeb198573ac98a892cad213e64ce7ed
c.jiayichen <c.jiayichen@gmail.com>
2021-12-08 16:56:59 -0500

    added logo

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="fixed-bottom text-end pe-2">
+      <h1 className="text-muted fw-lighter">note<strong>board</strong></h1>
+    </footer>
+  )
+}
